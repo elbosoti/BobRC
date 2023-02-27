1464329588b3802793b95c3b75002207f66df742
Bob B <bpblinde@live.nl>
2023-02-27 22:06:35 +0000

    Used ufw to allow socket port

--- BobRC_Server.py
+++ BobRC_Server.py
@@ -1,20 +1,16 @@
-import socket,threading,time,sys,cv2
-server_ip = ('10.14.1.5',5001)
-client_ip = ('0.0.0.0',5000)
+import socket,threading,time,sys
+server_ip = ('0.0.0.0',12000)
+car_ip = ('0.0.0.0',12001)
 
 
 
 def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(server_ip)
-    message, client = sock.recvfrom(1024)
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSock.bind(server_ip)
+    #serverSock.sendto(b'', server_ip)
+    message, client = serverSock.recvfrom(1024)
     print(message)
 
-def streamvideo():
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
-    cap.set(cv2.CAP_PROP_FPS, 30)
 
 
 if __name__ == "__main__":
