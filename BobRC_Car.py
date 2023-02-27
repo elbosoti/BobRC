e748de0c0dbe9083483b8e0fedc6b1f232321730
= <bpblinde@live.nl>
2023-02-27 14:14:50 -0700

    Got first communication to work

--- BobRC_Car.py
+++ BobRC_Car.py
@@ -1,17 +1,16 @@
 import socket,threading,time,sys,cv2
-server_ip = ('10.14.1.5',25501)
+server_ip = ('10.14.1.5',12000)
 
 
 
 def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(b'testing', server_ip)
+    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #clientSock.settimeout(1)
+    message = b'testing'
 
-def streamvideo():
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
-    cap.set(cv2.CAP_PROP_FPS, 30)
+    clientSock.sendto(message, server_ip)
+    
+
 
 
 if __name__ == "__main__":
