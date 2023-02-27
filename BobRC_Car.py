94f4baad784408d0f4baecca0f04163f5584834f
Bob B <bpblinde@live.nl>
2023-02-27 10:22:14 -0700

    Create Socket

--- BobRC_Car.py
+++ BobRC_Car.py
@@ -1,3 +1,11 @@
-import cv2
+import socket,threading,time,sys,cv2
+
+
+def main():
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def streamvideo():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 180)
+    cap.set(cv2.CAP_PROP_FPS, 30)
