57ba6809130b4efef85bfa02d816dc2a9d1ffb1e
Bob B <bpblinde@live.nl>
2023-03-13 20:29:12 -0600

    Unhardcoded the camera path

--- BobRC_Car.py
+++ BobRC_Car.py
@@ -13,12 +13,10 @@ print("server port bound")
 def send_video():
     pygame.init()
     pygame.camera.init()
-    cam = pygame.camera.Camera("/dev/video0", (320, 240), "RGB")
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
     cam.start()
     while True:
         sleep(0.0003)
-        s, address = CarSock.accept()
-        print("Got connection from", address)
         image = cam.get_image()
         CarSock.sendto(image, clientAddress)
 
