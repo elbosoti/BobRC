6ab87d7764458cc17b36864e2e04cc4c2760acd4
Bob B <bpblinde@live.nl>
2023-03-17 18:24:10 -0600

    Trying out the continuous method, bust

--- BobRC_Car.py
+++ BobRC_Car.py
@@ -1,25 +1,32 @@
+import socket
+import time
 import RPi.GPIO as GPIO
-from time import sleep
-import socket, pygame.camera
+
+from picamera2 import Picamera2, Preview 
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput
+
+
 clientAddress = ("10.14.1.50", 12000)
 address = ("0.0.0.0", 12001)
 GPIO.setmode(GPIO.BOARD) # Set pin mode to Board across entire program
 CarSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Global Socket variable
-CarSock.bind(address)
+# CarSock.bind(address)
 print("server port bound")
 
 
 
 def send_video():
-    pygame.init()
-    pygame.camera.init()
-    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
-    cam.start()
-    while True:
-        sleep(0.0003)
-        image = cam.get_image()
-        CarSock.sendto(image, clientAddress)
-
+    picam2 = Picamera2()
+    video_config = picam2.create_preview_configuration({"size": (640, 360)})
+    picam2.configure(video_config)
+    encoder = H264Encoder(1000000)
+    while(True):
+        CarSock.connect(clientAddress)
+        stream = sock.makefile("wb")
+        picam2.start_recording(encoder, FileOutput(stream))
+        time.sleep(20)
+        picam2.stop_recording()
 
 def receive_controls():
     while True:
@@ -30,8 +37,7 @@ def receive_controls():
             
     return
 
-def main():
+
+
+if (__name__ == "__main__"):
     send_video()
-    
-if __name__ == "__main__":
-    main()
