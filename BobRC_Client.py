443733af443a59646e682f85bc12f82919210d14
Bob B <bpblinde@live.nl>
2023-03-25 12:53:46 -0600

    Low latency stream up and running

--- BobRC_Client.py
+++ BobRC_Client.py
@@ -2,6 +2,7 @@ import socket
 from PIL import Image
 import BobRC
 import cv2, numpy
+import struct
 
 
 server_ip = ('0.0.0.0',12000)
@@ -11,42 +12,31 @@ client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP Route
 client_sock.bind(server_ip)
 
 
-
-def receive_array():
-    img = bytearray()
-    np_tile = [numpy.empty((90,80,3),dtype=numpy.uint8)]*8
-    pervious_index = 0
-    while True:
-        package = client_sock.recvfrom(65536)[0]
-        index = int.from_bytes(package[:1],'big')
-        if index < 8:
-            if pervious_index != index:#new tile
-                webp_tile = numpy.frombuffer(img, dtype=numpy.uint8)
-                try:
-                    np_tile[pervious_index] = cv2.imdecode(webp_tile,cv2.IMREAD_COLOR)#webp to np
-                    if pervious_index > index:#only show pic if all tiles are send 
-                        img_t  =numpy.concatenate((np_tile[0:4]),axis=1)#reassemble image together horizontally
-                        img_d  =numpy.concatenate((np_tile[4:8]),axis=1)#reassemble image together vertically
-                        img_al =numpy.concatenate((img_t,img_d)) #list of numpy arrays into single numpy array
-                        img_al =cv2.resize(img_al,(640,360))#upscale the image to get a better view
-                        cv2.imshow('videostream Open-ATS',img_al) #first arg is the name of the window; dont displays the picture
-                        cv2.waitKey(1)#displays the picture, waits 1ms
-                except:print('failed');pass;
-                img = bytearray()#reset img for next tile
-            pervious_index = index
-            img = img + package[1:]
-
-
 def receive_video():
-    img_str = b''
+    last_index = 0
+    img_tiles = [numpy.empty((90, 80, 3),dtype=numpy.uint8)]*8
+    img = bytearray()
     while True:
-        package = client_sock.recvfrom(65536)[0]
-        img_str += package
-        if len(img_str) == 230400:
-            frame = numpy.fromstring(img_str, dtype=numpy.uint8)
-            frame = frame.reshape (320,180,4)
-            cv2.imshow('BobRc',frame)
-            img_str = b''
+        package, addr = client_sock.recvfrom(65536)
+        index = struct.unpack("B",package[:1])[0]
+        if index != last_index:
+            try:
+                img_tiles[last_index] = cv2.imdecode( numpy.frombuffer(img, dtype=numpy.uint8), cv2.IMREAD_COLOR)
+                if index < last_index:
+                    img_x1 = numpy.concatenate((img_tiles[0:4]),axis=1)
+                    img_x2 = numpy.concatenate((img_tiles[4:8]),axis=1)
+                    img = numpy.concatenate((img_x1,img_x2))
+                    cv2.imshow('frame',  img)
+                    
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+            except Exception as e: print(e)
+            img = bytearray()
+        img = img + package[1:]
+        last_index = index
+
+
+        
 
 if __name__ == "__main__":
-    receive_video()
+    receive_video2()
