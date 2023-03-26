fc686fded29945427a2724d193c7f8a015850c35
Bob B <bpblinde@live.nl>
2023-03-26 16:20:59 -0600

    Added CarSpeed class

--- BobRC.py
+++ BobRC.py
@@ -9,3 +9,43 @@ def process_package(package):
     except Exception as e:
         print("Error processing package", e)
         return -1, None
+    
+class CarSpeed:
+    def __init__(self):
+        self.speed = 0
+        self.direction = 0
+    
+    def key_press(self, key):
+        if key == 'w':
+            self.speed = 1
+        elif key == 's':
+            self.speed = -1
+        elif key == 'a':
+            self.direction = -1
+        elif key == 'd':
+            self.direction = 1
+        elif key == ' ':
+            self.speed = 0
+            self.direction = 0
+        else:
+            return False
+        return True
+    
+    def key_depress(self, key):
+        if key == 'w' or key == 's':
+            self.speed = 0
+        elif key == 'a' or key == 'd':
+            self.direction = 0
+        else:
+            return False
+        return True
+    
+    def to_bytes(self):
+        return struct.pack("b", self.speed, self.direction)
+    
+    def from_bytes(self, data):
+        speed, direction = struct.unpack("b", data)
+        rc_car = CarSpeed()
+        rc_car.speed = self.speed
+        rc_car.direction = self.direction
+        return rc_car
