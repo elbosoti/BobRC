d83575fe08b2a8580bcbeef763983f0f46bae1b6
Bob B <bpblinde@live.nl>
2023-03-29 18:07:15 -0600

    Using BobRC more

--- BobRC_Car.py
+++ BobRC_Car.py
@@ -1,17 +1,17 @@
 import socket
 import time
-import io
+import threading
 import BobRC
 from picamera2 import Picamera2
 import cv2, numpy
 import struct
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 
 client_address = ("10.14.1.50", 12000)
-address = ("0.0.0.0", 12001)
-GPIO.setmode(GPIO.BOARD) # Set pin mode to Board across entire program
+address = ("0.0.0.0", 12000)
 car_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Global Socket variable
+car_sock.bind(address)
 
 def send_video():
     picam2 = Picamera2()
@@ -23,11 +23,23 @@ def send_video():
         img_array = picam2.capture_array()
         img_list = img_array.reshape(2, 90, 4, 80, 3).swapaxes(1,2)
 
-        for i, imgrow in enumerate(img_list):
-            for j, im_tile in enumerate(imgrow):
-                img_split_jpg = cv2.imencode('.webp', im_tile, [int(cv2.IMWRITE_WEBP_QUALITY),90])[1]
+        for i, img_row in enumerate(img_list):
+            for j, img_tile in enumerate(img_row):
+                img_split_jpg = cv2.imencode('.webp', img_tile, [int(cv2.IMWRITE_WEBP_QUALITY),90])[1]
                 car_sock.sendto(struct.pack("B", 4*i +j) + img_split_jpg.tobytes(), client_address)
 
+def receive_controls():
+    rc_car = BobRC.CarSpeed()
+    rc_car.set_pins(3, 5, 7, 11, 13, 15)
+    while True:
+        data, addr = car_sock.recvfrom(65536)
+        rc_car.from_bytes(data)
+        rc_car.update_motors()
+        
+
 if (__name__ == "__main__"):
-    send_video()
+    controls = threading.Thread(target=receive_controls)
+    video = threading.Thread(target=send_video)
+    controls.start()
+    video.start()
     
